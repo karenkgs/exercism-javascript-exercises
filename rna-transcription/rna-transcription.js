dea5d6b93718976a0aeadeb9e63241c94879381e
Karen Garcia dos Santos <karenkgs@gmail.com>
2018-01-10 11:10:28 -0200

    Refactoring the rna-transcription project, taking off prototype and adding Class

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,21 +1,20 @@
-var DnaTranscriber = function () {};
-
 const dnaToRnaTranscription = {
-	G:"C",
-	C:"G",
-	T:"A",
-	A:"U"
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U"
 };
 
-DnaTranscriber.prototype.toRna = function (dnaStrand) {
-	const dnaStrands = dnaStrand.split('');
-	const isValid = dnaStrands.every(dna => dnaToRnaTranscription[dna]);
+class DnaTranscriber {
+    toRna(dnaStrand) {
+        const dnaStrands = dnaStrand.split('');
+        const isValid = dnaStrands.every(dna => dnaToRnaTranscription[dna]);
 
-	if(!isValid) {
-		throw new Error("Invalid input");
-	}
+        if (!isValid) {
+            throw new Error("Invalid input");
+        }
 
-	return dnaStrands.map(dna => dnaToRnaTranscription[dna]).join("");
+        return dnaStrands.map(dna => dnaToRnaTranscription[dna]).join("");
+    }
 }
-
 module.exports = DnaTranscriber;
